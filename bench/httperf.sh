6576d55f769f7d958cdc78c6d153b11ae11ec535
crunch <crunchengine@gmail.com>
2015-04-05 18:04:35 -0400

    localhost address

--- bench/httperf.sh
+++ bench/httperf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-httperf --hog --server www.dhttpd.fr --port 8080 --uri /main/home.html --num-calls 1000 --num-conns 100
+httperf --hog --server 127.0.0.1 --port 8080 --uri /main/home.html --num-calls 1000 --num-conns 100
